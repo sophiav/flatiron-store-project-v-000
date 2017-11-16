166da99a977603272e39e0862df5b765d632ca70
Sophia Varella <sophiavarella@gmail.com>
2017-11-16 13:43:53 -0800

    Create migrations and model associations

--- db/migrate/20140805183012_create_items.rb
+++ db/migrate/20140805183012_create_items.rb
@@ -1,5 +1,11 @@
 class CreateItems < ActiveRecord::Migration
   def change
+    create_table :items do |t|
+      t.string :title
+      t.integer :price
+      t.integer :inventory
 
+      t.timestamps
+    end
   end
 end
